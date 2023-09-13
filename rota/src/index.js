11e39b166ef492356abb247f6966025754a1593d
NeryLVYZ <lucazvnery@gmail.com>
2023-09-13 17:04:27 -0300

    update

--- rota/src/index.js
+++ rota/src/index.js
@@ -8,7 +8,7 @@ import PaginaInicial from './paginas/PaginaIncial';
 import PaginaSobre from './paginas/PaginaSobre';
 
 
-const roteador = createBrowserRouter([
+const rotas = createBrowserRouter([
   {path:'/', element: <PaginaInicial/>},
   {path:'/sobre', element: <PaginaSobre/>}
 ]);
@@ -16,7 +16,7 @@ const roteador = createBrowserRouter([
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render( 
   <React.StrictMode>
-    <RouterProvider router={roteador}/>
+    <RouterProvider router={rotas}/>
   </React.StrictMode>
 );
 
